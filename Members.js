ac49e4bc92f6194f20ff4c10a8f15d1ed5c50744
Jake <gosnelljacob@gmail.com>
2019-09-11 21:37:46 -0500

    added all files of finished express js crash course to git

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Jerry Doe',
+    email: 'jerry@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Jeffrey Doe',
+    email: 'jeffrey@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 4,
+    name: 'Jimmy Doe',
+    email: 'jimmy@gmail.com',
+    status: 'active'
+  }
+]
+
+module.exports = members;
